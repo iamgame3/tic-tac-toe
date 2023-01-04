2df34fc592e623d8e0e286d0a63547b1b651cce5
iamgame3 <kosmosworld9@hotmail.com>
2023-01-03 20:30:15 -0500

    Add class to all game board squares, style their text, and create a function for filling the game board

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const gameBoard = (() => {
-
+    const currentBoard = ['X', 'O', '', '', '', '', 'X', '', 'O']
+    const gameBoxes = document.querySelectorAll('.game-square')
+    for (let i = 0; i < currentBoard.length; i += 1) {
+        gameBoxes[i].textContent = currentBoard[i]
+    }
     return {}
 })()
 
