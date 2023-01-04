83e62e4fd46a69b53700431630cc7895b00eab25
iamgame3 <kosmosworld9@hotmail.com>
2023-01-04 11:28:09 -0500

    Add a win check function, square check function, and changed all instances of 'box' to 'square'

--- script.js
+++ script.js
@@ -16,23 +16,84 @@ const gameBoard = (() => {
     // Make an array that represents the game board
     const currentBoard = ['', '', '', '', '', '', '', '', '']
     // Select all game squares and make it a variable
-    const gameBoxes = document.querySelectorAll('.game-square')
+    const gameSquares = document.querySelectorAll('.game-square')
+    // Check if a square is filled in
+    const squareCheck = (square) => square === 'X' || square === 'O'
+    // Check if a player has won
+    const winCheck = () => {
+        if (currentBoard[0] === 'X' && currentBoard[1] === 'X' && currentBoard[2] === 'X') {
+            uNameOneWin = true
+            console.log('Win!')
+        } else if (currentBoard[3] === 'X' && currentBoard[4] === 'X' && currentBoard[5] === 'X') {
+            uNameOneWin = true
+            console.log('Win!')
+        } else if (currentBoard[6] === 'X' && currentBoard[7] === 'X' && currentBoard[8] === 'X') {
+            uNameOneWin = true
+            console.log('Win!')
+        } else if (currentBoard[0] === 'X' && currentBoard[3] === 'X' && currentBoard[6] === 'X') {
+            uNameOneWin = true
+            console.log('Win!')
+        } else if (currentBoard[1] === 'X' && currentBoard[4] === 'X' && currentBoard[7] === 'X') {
+            uNameOneWin = true
+            console.log('Win!')
+        } else if (currentBoard[2] === 'X' && currentBoard[5] === 'X' && currentBoard[8] === 'X') {
+            uNameOneWin = true
+            console.log('Win!')
+        } else if (currentBoard[0] === 'X' && currentBoard[4] === 'X' && currentBoard[8] === 'X') {
+            uNameOneWin = true
+            console.log('Win!')
+        } else if (currentBoard[2] === 'X' && currentBoard[4] === 'X' && currentBoard[6] === 'X') {
+            uNameOneWin = true
+            console.log('Win!')
+        } else if (currentBoard[0] === 'O' && currentBoard[1] === 'O' && currentBoard[2] === 'O') {
+            uNameTwoWin = true
+            console.log('Win!')
+        } else if (currentBoard[3] === 'O' && currentBoard[4] === 'O' && currentBoard[5] === 'O') {
+            uNameTwoWin = true
+            console.log('Win!')
+        } else if (currentBoard[6] === 'O' && currentBoard[7] === 'O' && currentBoard[8] === 'O') {
+            uNameTwoWin = true
+            console.log('Win!')
+        } else if (currentBoard[0] === 'O' && currentBoard[3] === 'O' && currentBoard[6] === 'O') {
+            uNameTwoWin = true
+            console.log('Win!')
+        } else if (currentBoard[1] === 'O' && currentBoard[4] === 'O' && currentBoard[7] === 'O') {
+            uNameTwoWin = true
+            console.log('Win!')
+        } else if (currentBoard[2] === 'O' && currentBoard[5] === 'O' && currentBoard[8] === 'O') {
+            uNameTwoWin = true
+            console.log('Win!')
+        } else if (currentBoard[0] === 'O' && currentBoard[4] === 'O' && currentBoard[8] === 'O') {
+            uNameTwoWin = true
+            console.log('Win!')
+        } else if (currentBoard[2] === 'O' && currentBoard[4] === 'O' && currentBoard[6] === 'O') {
+            uNameTwoWin = true
+            console.log('Win!')
+        } else if (currentBoard.every(squareCheck)) {
+            // Check if all squares are filled in ^^^
+            console.log('Tie!')
+        }
+    }
     // Add X or O if the user clicks on a square
-    gameBoxes.forEach(box => box.addEventListener('click', () => {
+    gameSquares.forEach(square => square.addEventListener('click', () => {
         // Run if square is empty and it's the first player's turn
-        if (!box.textContent && uNameOneTurn) {
-            box.textContent = 'X'
+        if (!square.textContent && uNameOneTurn) {
+            square.textContent = 'X'
             uNameOneTurn = false
             uNameTwoTurn = true
             // Add turn to currentBoard array
-            currentBoard[box.getAttribute('data-square-number')] = 'X'
-        }   else if (!box.textContent && uNameTwoTurn) {
+            currentBoard[square.getAttribute('data-square-number')] = 'X'
+            // Check if the player won
+            winCheck()
+        }   else if (!square.textContent && uNameTwoTurn) {
             // Run if square is empty and it's the second player's turn ^^^
-            box.textContent = 'O'
+            square.textContent = 'O'
             uNameOneTurn = true
             uNameTwoTurn = false
             // Add turn to currentBoard array
-            currentBoard[box.getAttribute('data-square-number')] = 'O'
+            currentBoard[square.getAttribute('data-square-number')] = 'O'
+            // Check if the player won
+            winCheck()
         }
     }))
     // Make updated player turns global
