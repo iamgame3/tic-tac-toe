669bf0eb1f7a06bbb542de954b0329aa6ab06066
iamgame3 <kosmosworld9@hotmail.com>
2023-01-04 10:45:07 -0500

    Add game square data numbers and update currentBoard array everytime a square is filled in

--- script.js
+++ script.js
@@ -7,17 +7,19 @@ const players = (uNameOne, uNameTwo) => {
 const currentPlayers = players('John', 'Mary')
 
 const gameBoard = (() => {
-    const currentBoard = []
+    const currentBoard = ['', '', '', '', '', '', '', '', '']
     const gameBoxes = document.querySelectorAll('.game-square')
     gameBoxes.forEach(box => box.addEventListener('click', () => {
         if (!box.textContent && uNameOneTurn) {
             box.textContent = 'X'
             uNameOneTurn = false
             uNameTwoTurn = true
+            currentBoard[box.getAttribute('data-square-number')] = 'X'
         }   else if (!box.textContent && uNameTwoTurn) {
             box.textContent = 'O'
             uNameOneTurn = true
             uNameTwoTurn = false
+            currentBoard[box.getAttribute('data-square-number')] = 'O'
         }
     }))
     return {uNameOneTurn, uNameTwoTurn}
