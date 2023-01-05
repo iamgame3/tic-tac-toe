6a6457dcfc9125fdf173a49a593ae50cef837c99
iamgame3 <kosmosworld9@hotmail.com>
2023-01-05 14:15:43 -0500

    Add normal bot functionality and make win display text smaller

--- script.js
+++ script.js
@@ -25,18 +25,37 @@ const gameBoard = (() => {
     // Make empty variables that will be used later
     let uNameOneWin = ''
     let uNameTwoWin = ''
+    let botStatus = ''
     // Make an array that represents the game board
     let currentBoard = ['', '', '', '', '', '', '', '', '']
     // Select body container
     const bodyContainer = document.querySelector('.body-container')
     // Select reset button
     const resetButton = document.querySelector('.reset-button')
+    // Select bot buttons
+    const botNormalButton = document.querySelector('.bot-normal-button')
+    const botImpossibleButton = document.querySelector('.bot-impossible-button')
     // Select form confirm button
     const formConfirm = document.querySelector('.confirm-button')
     // Select all game squares and make it a variable
     const gameSquares = document.querySelectorAll('.game-square')
     // Check if a square is filled in
     const squareCheck = (square) => square === 'X' || square === 'O'
+    // Make normal bot function
+    const botNormal = () => {
+        // Get a random number between 0-8
+        let i = Math.round(Math.random()*8)
+        // Continue getting random numbers until number of currentBoard index is empty
+        while (currentBoard[i] !== '') {
+            i = Math.round(Math.random()*8)
+        }
+        gameSquares[i].textContent = 'O'
+        currentPlayers.uNameOneTurn = true
+        currentPlayers.uNameTwoTurn = false
+        currentBoard[i] = 'O'
+        // Check if bot won
+        winCheck()
+    }
     // Display win message
     const winMessage = (uNameOneW, uNameTwoW, uNameOne, uNameTwo) => {
         // Create win display div
@@ -76,6 +95,10 @@ const gameBoard = (() => {
         const winDisplay = document.querySelector('.win-display')
         if (winDisplay) {
             bodyContainer.removeChild(winDisplay)
+        }
+        if (botStatus === 'On' && currentPlayers.uNameTwoTurn) {setTimeout(() => {
+            botNormal()
+            }, 250)
         }
     }
     // Check if a player has won
@@ -133,6 +156,14 @@ const gameBoard = (() => {
             winMessage(false, false, currentPlayers.uNameOne, currentPlayers.uNameTwo)
         }
     }
+    // Turn bot on
+    const botOn = (bot) => {
+        gameReset()
+        document.getElementById('uNameTwo').value = `Bot (${bot})`
+        currentPlayers = players()
+        botStatus = 'On'
+        botDiff = bot
+    }
     // Add X or O if the user clicks on a square
     gameSquares.forEach(square => square.addEventListener('click', () => {
         // Run if square is empty and it's the first player's turn
@@ -146,6 +177,14 @@ const gameBoard = (() => {
             currentBoard[square.getAttribute('data-square-number')] = 'X'
             // Check if the player won
             winCheck()
+            // Check if bot is on
+            if (botStatus === 'On' && !currentBoard.every(squareCheck) && !(uNameOneWin || uNameTwoWin)) {
+                // Check bot difficulty and run respective functions after a short delay
+                if (botDiff === 'Normal') {setTimeout(() => {
+                    botNormal()
+                    }, 250)
+                }
+            }
         }   else if (!square.textContent && currentPlayers.uNameTwoTurn) {
             // ^^^ Run if square is empty and it's the second player's turn
             square.textContent = 'O'
@@ -157,8 +196,16 @@ const gameBoard = (() => {
             winCheck()
         }
     }))
+    // Turn on bot when bot buttons are clicked
+    botNormalButton.addEventListener('click', () => {
+        botOn("Normal")
+    })
+    botImpossibleButton.addEventListener('click', () => {
+        botOn("Impossible")
+    })
     // Clear winner and game board if reset button is clicked
     resetButton.addEventListener('click', gameReset)
+    // Change player names when form confirm button is clicked
     formConfirm.addEventListener('click', () => {
         currentPlayers = players()
     })
