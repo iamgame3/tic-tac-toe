737d05ec90f604bc55b37fc560f8a100fdae0e63
iamgame3 <kosmosworld9@hotmail.com>
2023-01-07 13:49:12 -0500

    Add unbeatable bot, edit code, and rearrange code

--- script.js
+++ script.js
@@ -22,10 +22,14 @@ let currentPlayers = players()
 
 // Create game board logic
 const gameBoard = (() => {
-    // Make empty variables that will be used later
+    // Make empty variables that will be used later so errors do not occur
     let uNameOneWin = ''
     let uNameTwoWin = ''
+    let tie = ''
     let botStatus = ''
+    let botDiff = ''
+    const bestMove = ''
+    let bestMoveFinal = ''
     // Make an array that represents the game board
     let currentBoard = ['', '', '', '', '', '', '', '', '']
     // Select body container
@@ -41,23 +45,8 @@ const gameBoard = (() => {
     const gameSquares = document.querySelectorAll('.game-square')
     // Check if a square is filled in
     const squareCheck = (square) => square === 'X' || square === 'O'
-    // Make normal bot function
-    const botNormal = () => {
-        // Get a random number between 0-8
-        let i = Math.round(Math.random()*8)
-        // Continue getting random numbers until number of currentBoard index is empty
-        while (currentBoard[i] !== '') {
-            i = Math.round(Math.random()*8)
-        }
-        gameSquares[i].textContent = 'O'
-        currentPlayers.uNameOneTurn = true
-        currentPlayers.uNameTwoTurn = false
-        currentBoard[i] = 'O'
-        // Check if bot won
-        winCheck()
-    }
     // Display win message
-    const winMessage = (uNameOneW, uNameTwoW, uNameOne, uNameTwo) => {
+    const winMessage = (uNameOneW, uNameTwoW, tied, uNameOne, uNameTwo) => {
         // Create win display div
         const winDisplay = document.createElement('div')
         winDisplay.classList.add('win-display')
@@ -66,11 +55,168 @@ const gameBoard = (() => {
             winDisplay.textContent = `${uNameOne} is the winner!`
         } else if (uNameTwoW) {
             winDisplay.textContent = `${uNameTwo} is the winner!`
-        } else {
+        } else if (tied) {
             winDisplay.textContent = "Cat's game!"
         }
         // Add finished display message to the page
         bodyContainer.appendChild(winDisplay)
+    }
+    // Check if a player has won or if it was a tie
+    const winCheck = () => {
+        if (currentBoard[0] === 'X' && currentBoard[1] === 'X' && currentBoard[2] === 'X') {
+            uNameOneWin = true
+        } else if (currentBoard[3] === 'X' && currentBoard[4] === 'X' && currentBoard[5] === 'X') {
+            uNameOneWin = true
+        } else if (currentBoard[6] === 'X' && currentBoard[7] === 'X' && currentBoard[8] === 'X') {
+            uNameOneWin = true
+        } else if (currentBoard[0] === 'X' && currentBoard[3] === 'X' && currentBoard[6] === 'X') {
+            uNameOneWin = true
+        } else if (currentBoard[1] === 'X' && currentBoard[4] === 'X' && currentBoard[7] === 'X') {
+            uNameOneWin = true
+        } else if (currentBoard[2] === 'X' && currentBoard[5] === 'X' && currentBoard[8] === 'X') {
+            uNameOneWin = true
+        } else if (currentBoard[0] === 'X' && currentBoard[4] === 'X' && currentBoard[8] === 'X') {
+            uNameOneWin = true
+        } else if (currentBoard[2] === 'X' && currentBoard[4] === 'X' && currentBoard[6] === 'X') {
+            uNameOneWin = true
+        } else if (currentBoard[0] === 'O' && currentBoard[1] === 'O' && currentBoard[2] === 'O') {
+            uNameTwoWin = true
+        } else if (currentBoard[3] === 'O' && currentBoard[4] === 'O' && currentBoard[5] === 'O') {
+            uNameTwoWin = true
+        } else if (currentBoard[6] === 'O' && currentBoard[7] === 'O' && currentBoard[8] === 'O') {
+            uNameTwoWin = true
+        } else if (currentBoard[0] === 'O' && currentBoard[3] === 'O' && currentBoard[6] === 'O') {
+            uNameTwoWin = true
+        } else if (currentBoard[1] === 'O' && currentBoard[4] === 'O' && currentBoard[7] === 'O') {
+            uNameTwoWin = true
+        } else if (currentBoard[2] === 'O' && currentBoard[5] === 'O' && currentBoard[8] === 'O') {
+            uNameTwoWin = true
+        } else if (currentBoard[0] === 'O' && currentBoard[4] === 'O' && currentBoard[8] === 'O') {
+            uNameTwoWin = true
+        } else if (currentBoard[2] === 'O' && currentBoard[4] === 'O' && currentBoard[6] === 'O') {
+            uNameTwoWin = true
+        } else if (currentBoard.every(squareCheck)) {
+            // ^^^ Check if all squares are filled in
+            tie = true
+        }
+    }
+    // Make normal bot function
+    const botNormal = () => {
+        // Get a random number between 0-8
+        let i = Math.round(Math.random()*8)
+        // Continue getting random numbers until number generated corresponds to an empty currentBoard index
+        while (currentBoard[i] !== '') {
+            i = Math.round(Math.random()*8)
+        }
+        // When a vaild spot is detected, bot goes and it's the user's turn
+        gameSquares[i].textContent = 'O'
+        currentBoard[i] = 'O'
+        currentPlayers.uNameOneTurn = true
+        currentPlayers.uNameTwoTurn = false
+        // Check if the bot won or if it was a tie
+        winCheck()
+        if (uNameTwoWin) {
+            winMessage(false, true, false, currentPlayers.uNameOne, currentPlayers.uNameTwo)
+        }
+        if (tie) {
+            winMessage(false, false, true, currentPlayers.uNameOne, currentPlayers.uNameTwo)
+        }
+    }
+    // Make minimax algorithm function
+    const minimax = (board, depth, maximizingPlayer) => {
+        // Check if anybody won or if it was a tie, and if it anything comes up as true give that path a score
+        winCheck()
+        const scores = {
+            X: 1,
+            O: -1,
+            Tie: 0
+        }
+        if (uNameOneWin || uNameTwoWin || tie) {
+            if (uNameOneWin) {
+                score = scores.X
+            } else if (uNameTwoWin) {
+                score = scores.O
+            } else score = scores.Tie
+            // Reset end game values and return score
+            uNameOneWin = false
+            uNameTwoWin = false
+            tie = false
+            return score
+        }
+        // If it's the user's turn, execute
+        if (maximizingPlayer) {
+            // Make the best move negative infinity so any other number is better
+            let bestMove = -Infinity
+            // For each spot, make a move and recursively run minimax until bestMove is decided based on what score's are returned
+            for (let i = 0; i < 9; i += 1) {
+                // Check if spot is taken
+                if (currentBoard[i] === '') {
+                    currentBoard[i] = 'X'
+                    // Run minimax again with move made and make it the other player's turn
+                    const move = minimax(currentBoard, depth + 1, false)
+                    currentBoard[i] = ''
+                    // Find the best move, which is the highest number since this is the maximizing player's turn
+                    bestMove = Math.max(bestMove, move)
+                }
+            }
+            return bestMove
+        }
+        // If it's the bot's turn, execute
+        if (!maximizingPlayer) {
+            // Make the best move infinity so any other number is better
+            let bestMove = Infinity
+            // For each spot, make a move and recursively run minimax until bestMove is decided based on what score's are returned
+            for (let i = 0; i < 9; i += 1) {
+                if (currentBoard[i] === '') {
+                    currentBoard[i] = 'O'
+                    // Run minimax again with move made and make it the other player's turn
+                    const move = minimax(currentBoard, depth + 1, true)
+                    currentBoard[i] = ''
+                    // Find the best move, which is the lowest number since this is the minimizing player's turn
+                    bestMove = Math.min(bestMove, move)
+                }
+            }
+            return bestMove
+        }
+    }
+    // Make impossible bot function
+    const botImpossible = () => {
+        // Make the best move negative infinity so any other number is better
+        let bestMove = Infinity
+        // For each spot, make a move and recursively run minimax until bestMove is decided based on what score's are returned
+        for (let i = 0; i < 9; i += 1) {
+            // Check if spot is taken
+            if (currentBoard[i] === '') {
+                currentBoard[i] = 'O'
+                // Run minimax again with move made and make it the other player's turn
+                const move = minimax(currentBoard, 0, true)
+                currentBoard[i] = ''
+                // Find the best move, which is the lowest number since this is the minimizing player's turn
+                if (move < bestMove) {
+                    bestMove = move
+                    // Assign this spot as the best move for the bot
+                    bestMoveFinal = i
+                }
+            }
+        }
+        // Reset end game values
+        uNameOneWin = false
+        uNameTwoWin = false
+        tie = false
+        // Bot makes turn with best possible move
+        currentBoard[bestMoveFinal] = 'O'
+        gameSquares[bestMoveFinal].textContent = 'O'
+        // Make it the user's turn
+        currentPlayers.uNameOneTurn = true
+        currentPlayers.uNameTwoTurn = false
+        // Check if the bot won or if it was a tie
+        winCheck()
+        if (uNameTwoWin) {
+            winMessage(false, true, false, currentPlayers.uNameOne, currentPlayers.uNameTwo)
+        }
+        if (tie) {
+            winMessage(false, false, true, currentPlayers.uNameOne, currentPlayers.uNameTwo)
+        }
     }
     // Reset the game board and game information
     const gameReset = () => {
@@ -82,9 +228,10 @@ const gameBoard = (() => {
             currentPlayers.uNameOneTurn = true
             currentPlayers.uNameTwoTurn = false
         }
-        // Set win status to false for both players
+        // Reset end game values
         uNameOneWin = false
         uNameTwoWin = false
+        tie = false
         // Reset board array
         currentBoard = ['', '', '', '', '', '', '', '', '']
         // Clear all squares
@@ -96,69 +243,19 @@ const gameBoard = (() => {
         if (winDisplay) {
             bodyContainer.removeChild(winDisplay)
         }
+        // If the bot is active and it's the bot's turn, make the bot play the starting move
         if (botStatus === 'On' && currentPlayers.uNameTwoTurn) {setTimeout(() => {
-            botNormal()
+            // Check bot difficulty and run respective functions after a short delay
+            if (botDiff === 'Normal') {
+                botNormal()
+            } else botImpossible()
             }, 250)
-        }
-    }
-    // Check if a player has won
-    const winCheck = () => {
-        if (currentBoard[0] === 'X' && currentBoard[1] === 'X' && currentBoard[2] === 'X') {
-            uNameOneWin = true
-            winMessage(true, false, currentPlayers.uNameOne, currentPlayers.uNameTwo)
-        } else if (currentBoard[3] === 'X' && currentBoard[4] === 'X' && currentBoard[5] === 'X') {
-            uNameOneWin = true
-            winMessage(true, false, currentPlayers.uNameOne, currentPlayers.uNameTwo)
-        } else if (currentBoard[6] === 'X' && currentBoard[7] === 'X' && currentBoard[8] === 'X') {
-            uNameOneWin = true
-            winMessage(true, false, currentPlayers.uNameOne, currentPlayers.uNameTwo)
-        } else if (currentBoard[0] === 'X' && currentBoard[3] === 'X' && currentBoard[6] === 'X') {
-            uNameOneWin = true
-            winMessage(true, false, currentPlayers.uNameOne, currentPlayers.uNameTwo)
-        } else if (currentBoard[1] === 'X' && currentBoard[4] === 'X' && currentBoard[7] === 'X') {
-            uNameOneWin = true
-            winMessage(true, false, currentPlayers.uNameOne, currentPlayers.uNameTwo)
-        } else if (currentBoard[2] === 'X' && currentBoard[5] === 'X' && currentBoard[8] === 'X') {
-            uNameOneWin = true
-            winMessage(true, false, currentPlayers.uNameOne, currentPlayers.uNameTwo)
-        } else if (currentBoard[0] === 'X' && currentBoard[4] === 'X' && currentBoard[8] === 'X') {
-            uNameOneWin = true
-            winMessage(true, false, currentPlayers.uNameOne, currentPlayers.uNameTwo)
-        } else if (currentBoard[2] === 'X' && currentBoard[4] === 'X' && currentBoard[6] === 'X') {
-            uNameOneWin = true
-            winMessage(true, false, currentPlayers.uNameOne, currentPlayers.uNameTwo)
-        } else if (currentBoard[0] === 'O' && currentBoard[1] === 'O' && currentBoard[2] === 'O') {
-            uNameTwoWin = true
-            winMessage(false, true, currentPlayers.uNameOne, currentPlayers.uNameTwo)
-        } else if (currentBoard[3] === 'O' && currentBoard[4] === 'O' && currentBoard[5] === 'O') {
-            uNameTwoWin = true
-            winMessage(false, true, currentPlayers.uNameOne, currentPlayers.uNameTwo)
-        } else if (currentBoard[6] === 'O' && currentBoard[7] === 'O' && currentBoard[8] === 'O') {
-            uNameTwoWin = true
-            winMessage(false, true, currentPlayers.uNameOne, currentPlayers.uNameTwo)
-        } else if (currentBoard[0] === 'O' && currentBoard[3] === 'O' && currentBoard[6] === 'O') {
-            uNameTwoWin = true
-            winMessage(false, true, currentPlayers.uNameOne, currentPlayers.uNameTwo)
-        } else if (currentBoard[1] === 'O' && currentBoard[4] === 'O' && currentBoard[7] === 'O') {
-            uNameTwoWin = true
-            winMessage(false, true, currentPlayers.uNameOne, currentPlayers.uNameTwo)
-        } else if (currentBoard[2] === 'O' && currentBoard[5] === 'O' && currentBoard[8] === 'O') {
-            uNameTwoWin = true
-            winMessage(false, true, currentPlayers.uNameOne, currentPlayers.uNameTwo)
-        } else if (currentBoard[0] === 'O' && currentBoard[4] === 'O' && currentBoard[8] === 'O') {
-            uNameTwoWin = true
-            winMessage(false, true, currentPlayers.uNameOne, currentPlayers.uNameTwo)
-        } else if (currentBoard[2] === 'O' && currentBoard[4] === 'O' && currentBoard[6] === 'O') {
-            uNameTwoWin = true
-            winMessage(false, true, currentPlayers.uNameOne, currentPlayers.uNameTwo)
-        } else if (currentBoard.every(squareCheck)) {
-            // ^^^ Check if all squares are filled in
-            winMessage(false, false, currentPlayers.uNameOne, currentPlayers.uNameTwo)
         }
     }
     // Turn bot on
     const botOn = (bot) => {
         gameReset()
+        // Make the second player the bot
         document.getElementById('uNameTwo').value = `Bot (${bot})`
         currentPlayers = players()
         botStatus = 'On'
@@ -166,34 +263,50 @@ const gameBoard = (() => {
     }
     // Add X or O if the user clicks on a square
     gameSquares.forEach(square => square.addEventListener('click', () => {
-        // Run if square is empty and it's the first player's turn
-        if (uNameOneWin || uNameTwoWin) {
+        // If the game has finished, reset the board
+        if (uNameOneWin || uNameTwoWin || tie) {
             gameReset()
         } else if (!square.textContent && currentPlayers.uNameOneTurn) {
+            // ^^^ If the square is empty and it's the first player's turn, execute
             square.textContent = 'X'
-            currentPlayers.uNameOneTurn = false
-            currentPlayers.uNameTwoTurn = true
             // Add turn to currentBoard array
             currentBoard[square.getAttribute('data-square-number')] = 'X'
-            // Check if the player won
+            currentPlayers.uNameOneTurn = false
+            currentPlayers.uNameTwoTurn = true
+            // Check if the player won or if it was a tie
             winCheck()
-            // Check if bot is on
-            if (botStatus === 'On' && !currentBoard.every(squareCheck) && !(uNameOneWin || uNameTwoWin)) {
+            if (uNameOneWin) {
+                winMessage(true, false, false, currentPlayers.uNameOne, currentPlayers.uNameTwo)
+            }
+            if (tie) {
+                winMessage(false, false, true, currentPlayers.uNameOne, currentPlayers.uNameTwo)
+            }
+            // If the bot is active and the game hasn't finished, execute
+            if (botStatus === 'On' && !(uNameOneWin || uNameTwoWin || tie)) {
                 // Check bot difficulty and run respective functions after a short delay
                 if (botDiff === 'Normal') {setTimeout(() => {
                     botNormal()
+                    }, 250)
+                } else {setTimeout(() => {
+                    botImpossible()
                     }, 250)
                 }
             }
         }   else if (!square.textContent && currentPlayers.uNameTwoTurn) {
             // ^^^ Run if square is empty and it's the second player's turn
             square.textContent = 'O'
-            currentPlayers.uNameOneTurn = true
-            currentPlayers.uNameTwoTurn = false
             // Add turn to currentBoard array
             currentBoard[square.getAttribute('data-square-number')] = 'O'
-            // Check if the player won
+            currentPlayers.uNameOneTurn = true
+            currentPlayers.uNameTwoTurn = false
+            // Check if the player won or if it was a tie
             winCheck()
+            if (uNameTwoWin) {
+                winMessage(false, true, false, currentPlayers.uNameOne, currentPlayers.uNameTwo)
+            }
+            if (tie) {
+                winMessage(false, false, true, currentPlayers.uNameOne, currentPlayers.uNameTwo)
+            }
         }
     }))
     // Turn on bot when bot buttons are clicked
